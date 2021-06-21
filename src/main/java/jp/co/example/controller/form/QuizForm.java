8255f44e657097c395567d6c5c93c0f664d7db75
koh <21tamayose_ko@axiz.co.jp>
2021-06-21 13:48:07 +0900

    クイズコントローラ作成

--- src/main/java/jp/co/example/controller/form/QuizForm.java
+++ src/main/java/jp/co/example/controller/form/QuizForm.java
@@ -15,5 +15,78 @@ public class QuizForm{
 	private String caztegoryName;
 	private Integer quizNum;
 
+	public Integer getQuizId() {
+		return quizId;
+	}
+	public void setQuizId(Integer quizId) {
+		this.quizId = quizId;
+	}
+	public Integer getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(Integer categoryId) {
+		this.categoryId = categoryId;
+	}
+	public String getQuizTitle() {
+		return quizTitle;
+	}
+	public void setQuizTitle(String quizTitle) {
+		this.quizTitle = quizTitle;
+	}
+	public String getQuizStatement() {
+		return quizStatement;
+	}
+	public void setQuizStatement(String quizStatement) {
+		this.quizStatement = quizStatement;
+	}
+	public Integer getCorrectAnswer() {
+		return correctAnswer;
+	}
+	public void setCorrectAnswer(Integer correctAnswer) {
+		this.correctAnswer = correctAnswer;
+	}
+	public String getCommentary() {
+		return commentary;
+	}
+	public void setCommentary(String commentary) {
+		this.commentary = commentary;
+	}
+	public Integer getDisplay() {
+		return display;
+	}
+	public void setDisplay(Integer display) {
+		this.display = display;
+	}
+	public Integer getQuizChoiceId() {
+		return quizChoiceId;
+	}
+	public void setQuizChoiceId(Integer quizChoiceId) {
+		this.quizChoiceId = quizChoiceId;
+	}
+	public String getChoice() {
+		return choice;
+	}
+	public void setChoice(String choice) {
+		this.choice = choice;
+	}
+	public Integer getMode() {
+		return mode;
+	}
+	public void setMode(Integer mode) {
+		this.mode = mode;
+	}
+	public String getCaztegoryName() {
+		return caztegoryName;
+	}
+	public void setCaztegoryName(String caztegoryName) {
+		this.caztegoryName = caztegoryName;
+	}
+	public Integer getQuizNum() {
+		return quizNum;
+	}
+	public void setQuizNum(Integer quizNum) {
+		this.quizNum = quizNum;
+	}
+
 
 }
---
+++ src/main/java/jp/co/example/service/QuizService.java
@@ -0,0 +1,5 @@
+package jp.co.example.service;
+
+public interface QuizService {
+
+}
