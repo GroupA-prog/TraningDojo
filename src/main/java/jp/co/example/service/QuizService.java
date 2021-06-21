8205b7f2d4cc5b2851f77873ef5c4ebca54f3768
koh <21tamayose_ko@axiz.co.jp>
2021-06-21 19:37:39 +0900

    クイズコントローラ削除

--- src/main/java/jp/co/example/service/QuizService.java
+++ src/main/java/jp/co/example/service/QuizService.java
@@ -8,14 +8,6 @@ public interface QuizService {
 	public List<Quiz> findByCategoryQuiz(Integer categoryId, Integer quizNum);
 	public List<Quiz> findByRankCategory(Integer categoryId);
 	public List<Quiz> findByQuizTitle(String quizTitle);
-<<<<<<< HEAD
-=======
-
->>>>>>> branch 'main' of https://github.com/GroupA-prog/TraningDojo.git
 	public String selectMode(Integer modeId);
-<<<<<<< HEAD
-=======
-
->>>>>>> branch 'main' of https://github.com/GroupA-prog/TraningDojo.git
 	public List<Quiz> insertQuiz(Integer categoryId, String quizTitle, String quizStatment, Integer correctAnswer, String commentary, Integer display);
 }
