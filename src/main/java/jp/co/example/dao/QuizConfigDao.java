10e8a44dc38dd17d355a3540b12de0ffa6b67ba3
naoto <naoto19990119@i.softbank.jp>
2021-06-21 14:31:34 +0900

    変更

---
+++ src/main/java/jp/co/example/dao/QuizConfigDao.java
@@ -0,0 +1,11 @@
+package jp.co.example.dao;
+
+import java.util.List;
+
+import jp.co.example.dto.entity.Category;
+
+public interface QuizConfigDao {
+
+	public List<Category> categoryNameAll();
+
+}
