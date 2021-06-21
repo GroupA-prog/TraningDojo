cb2c8986680767da82b1ac4b23b3cdf21983621d
shimojou <21shimojo_yuuri@axiz.co.jp>
2021-06-21 18:08:04 +0900

    ああ

--- src/main/java/jp/co/example/controller/SasakiController.java
+++ src/main/java/jp/co/example/controller/SasakiController.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,7 +14,7 @@ import jp.co.example.controller.form.QuizForm;
 import jp.co.example.dto.entity.Category;
 import jp.co.example.service.QuizConfigService;
 
-@Controller
+//@Controller
 public class SasakiController {
 
 	@Autowired
