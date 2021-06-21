e9b429d18f7f19d8f5e2d7af9e1c3d10c2e0dd4b
Riku Tsukayama <21tsukayama_riku@axiz.co.jp>
2021-06-21 15:55:27 +0900

    QuizServiceのコンフリクト編集

--- src/main/java/jp/co/example/service/impl/QuizServiceImpl.java
+++ src/main/java/jp/co/example/service/impl/QuizServiceImpl.java
@@ -19,13 +19,11 @@ public class QuizServiceImpl implements QuizService{
 		return quizDao.findByCategoryQuiz(categoryId, quizNum);
 	}
 
-<<<<<<< HEAD
+
 	public List<Quiz> findByRankCategory(Integer categoryId){
 		return quizDao.findByRankCategory(categoryId);
-=======
-	public List<Quiz> findByRankCategory(Integer categoryId) {
-		return null;
 	}
+
 
 	@Override
 	public List<Quiz> findByQuizTitle(String quizTitle) {
@@ -35,7 +33,6 @@ public class QuizServiceImpl implements QuizService{
 	@Override
 	public int insertQuiz(Integer categoryId, String quizTitle, String quizStatment, Integer correctAnswer, String commentary, Integer display) {
 		return quizDao.insertQuiz(categoryId, quizTitle, quizStatment, correctAnswer, commentary, display);
->>>>>>> branch 'main' of https://github.com/GroupA-prog/TraningDojo.git
 	}
 
 }
