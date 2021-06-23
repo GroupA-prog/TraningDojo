22bcec16540b04e2d0f2dabb22d730bc8ad935e1
koh <21tamayose_ko@axiz.co.jp>
2021-06-23 17:05:22 +0900

    クイズ全

--- src/main/java/jp/co/example/service/QuizService.java
+++ src/main/java/jp/co/example/service/QuizService.java
@@ -9,6 +9,7 @@ public interface QuizService {
 	public List<List<Quiz>> findByRankCategory(Integer categoryId);
 	public List<List<Integer>> answerList(Integer quizNum);
 	public void answerUpdate(List<List<Integer>> answer,Integer quizIndex, List<Integer> choiceId);
+	public void choiceUpdate(List<Integer> choiceList, Integer choice1,Integer choice2,Integer choice3,Integer choice4,Integer choice5);
 	public List<Quiz> findByQuizTitle(Integer categoryId, String quizTitle);
 	public List<Integer> scoring(List<List<Quiz>> quizList,List<List<Integer>> answerList);
 	public String selectMode(Integer modeId);
