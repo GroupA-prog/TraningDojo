374d5a9c4a9037097bd86167c4ed26bcf9a9f80f
ishihara <21ishihara_saki@axiz.co.jp>
2021-06-24 18:49:33 +0900

    履歴のフォーム変更

--- src/main/java/jp/co/example/controller/form/LogForm.java
+++ src/main/java/jp/co/example/controller/form/LogForm.java
@@ -1,8 +1,13 @@
 package jp.co.example.controller.form;
 
+import java.util.List;
+
 public class LogForm {
-	//カテゴリ選択
-	private Integer categoryId;
+	private Integer categoryId;	//カテゴリ選択
+	private Integer historyId;		//履歴選択
+	private List<String> comment;		//コメント入力
+	private List<Integer> historyDetailId;
+	private Integer updateTime;
 
 	public Integer getCategoryId() {
 		return categoryId;
@@ -11,6 +16,40 @@ public class LogForm {
 	public void setCategoryId(Integer categoryId) {
 		this.categoryId = categoryId;
 	}
+
+	public Integer getHistoryId() {
+		return historyId;
+	}
+
+	public void setHistoryId(Integer historyId) {
+		this.historyId = historyId;
+	}
+
+	public List<String> getComment() {
+		return comment;
+	}
+
+	public void setComment(List<String> comment) {
+		this.comment = comment;
+	}
+
+	public List<Integer> getHistoryDetailId() {
+		return historyDetailId;
+	}
+
+	public void setHistoryDetailId(List<Integer> historyDetailId) {
+		this.historyDetailId = historyDetailId;
+	}
+
+	public Integer getUpdateTime() {
+		return updateTime;
+	}
+
+	public void setUpdateTime(Integer updateTime) {
+		this.updateTime = updateTime;
+	}
+
+
 
 
 }
