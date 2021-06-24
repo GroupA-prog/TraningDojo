5a0f12223a9101cdcc1ecff9c8e39a98432d5f99
shimojou <21shimojo_yuuri@axiz.co.jp>
2021-06-24 19:17:13 +0900

    aaa

--- src/main/java/jp/co/example/dto/entity/UserInfo.java
+++ src/main/java/jp/co/example/dto/entity/UserInfo.java
@@ -6,41 +6,51 @@ public class UserInfo {
 	private String password;
 	private String userName;
 	private Integer role;
+
 	public Integer getUserId() {
 		return userId;
 	}
+
 	public void setUserId(Integer userId) {
 		this.userId = userId;
 	}
+
 	public String getLoginId() {
 		return loginId;
 	}
+
 	public void setLoginId(String loginId) {
 		this.loginId = loginId;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	public String getUserName() {
 		return userName;
 	}
+
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
+
 	public Integer getRole() {
 		return role;
 	}
+
 	public void setRole(Integer role) {
 		this.role = role;
 	}
+
 	@Override
 	public String toString() {
 		return "UserInfo [userId=" + userId + ", loginId=" + loginId + ", password=" + password + ", userName="
 				+ userName + ", role=" + role + "]";
 	}
-
 
 }
